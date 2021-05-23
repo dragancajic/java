5ad29996b4faa0fc178642200fc9ecd41513301b
Dragan Cajic <cajic_dragan@yahoo.com>
2021-05-23 23:22:59 +0200

    find index of smallest number in list

---
+++ IndexOfSmallest.java
@@ -0,0 +1,55 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOfSmallest {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// implement here a program that reads user input
+		// until the user enters 9999
+		// after that, the program prints the smallest number
+		// and its index -- the smallest number
+		// might appear multiple times
+		ArrayList<Integer> numbers = new ArrayList<>();
+		
+		while (true) {
+			int number = scanner.nextInt();
+			
+			if (number == 9999) {
+				break;
+			}
+			numbers.add(number);
+		}
+		
+		System.out.println(); // empty line
+		
+		int smallest = findSmallest(numbers);
+		System.out.println("Smallest number: " + smallest);
+		findIndex(numbers, smallest);
+	}
+
+	// implement finding the smallest number in the list
+	public static int findSmallest(ArrayList<Integer> list) {
+		int min = list.get(0);
+		
+		int index = 0;
+		while (index < list.size()) {
+			if (list.get(index) < min) {
+				min = list.get(index);
+			}
+			index++;
+		}
+		return min;
+	}
+
+	// implement finding the index of smallest number
+	public static void findIndex(ArrayList<Integer> list, int value) {
+		for (int index = 0; index < list.size(); index++) {
+			if (list.get(index) == value) {
+				System.out.println("Found at index " + index);
+			}
+		}
+	}
+}
