78b760ae0030743569a711b8d6606cd04a624917
Dragan Cajic <cajic_dragan@yahoo.com>
2021-05-29 12:16:38 +0200

    use increment instead of decrement / better reasoning (logical thinking)! ;-)

--- Factorial.java
+++ Factorial.java
@@ -13,15 +13,24 @@ public class Factorial {
 		// Factorial of 4: f(4) ---> 4! = 1 * 2 * 3 * 4 = 24
 		// Factorial of 0: f(0) ---> 0! = 1
 		
+		// TODO: Try to constraint given number to get computable factorials!
 		System.out.print("Give a number? ");
 		//int number = Integer.valueOf(scanner.nextLine()); // Redundant boxing!
 		int number = scanner.nextInt();
 		
-		long factorial = 1;
-		
+		long factorial = 1L;
+		/*
 		while (number > 0) {
 			factorial *= number;
 			number--;
+		}
+		*/
+		// use increment instead of decrement for better reasoning
+		// (logical thinking) and easier understanding of program! ;-)
+		int i = 1;
+		while (i <= number) {
+			factorial *= i;
+			i++;
 		}
 		
 		if (number < 0) {
